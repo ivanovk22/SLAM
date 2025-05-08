b90a116e5999a72fc9fce87eefd71aed4c188d3d
OzanOezel <ozzozel@gmail.com>
2025-05-08 11:34:38 +0200

    test

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('Hello2')
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.linalg import block_diag
+
+plt.close('all')
+
+d = np.load('data_point_land_1.npz', allow_pickle=True)
+Meas = d['Meas']
+Vtn = d['Uf']
+Wtn = d['Ua']
+
+
+
+
+#enviroenment
+sizeenv = 27.
